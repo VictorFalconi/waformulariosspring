bb2efde697e806743d031d941ca23d1d9aead390
VictorFalconi <v.falconi.m@gmail.com>
2024-04-11 22:46:20 -0500

    Login

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -1,12 +1,27 @@
 package pe.edu.cibertec.waformulariosspring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import pe.edu.cibertec.waformulariosspring.model.LoginModel;
 
 @Controller
 public class LoginController {
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("loginModel", new LoginModel());
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String validarLogin(@ModelAttribute("loginModel") LoginModel loginModel, Model model){
+        if(loginModel.getUsuario().equals("vfalconim") &&
+        loginModel.getPassword().equals("123")) {
+            return "home";
+        }
         return "login";
     }
 }
