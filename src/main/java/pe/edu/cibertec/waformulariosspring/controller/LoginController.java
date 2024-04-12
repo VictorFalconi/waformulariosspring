bf809a83ba6a6738700ac0d563be2d5d8aaba38d
VictorFalconi <v.falconi.m@gmail.com>
2024-04-11 23:03:33 -0500

    Ejercicio login terminado

--- src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
+++ src/main/java/pe/edu/cibertec/waformulariosspring/controller/LoginController.java
@@ -12,16 +12,22 @@ public class LoginController {
     @GetMapping("/login")
     public String login(Model model){
         model.addAttribute("loginModel", new LoginModel());
-
+        model.addAttribute("mostrarMensaje", false);
         return "login";
     }
 
     @PostMapping("/login")
-    public String validarLogin(@ModelAttribute("loginModel") LoginModel loginModel, Model model){
+    public String validarLogin
+            (@ModelAttribute("loginModel") LoginModel loginModel, Model model){
         if(loginModel.getUsuario().equals("vfalconim") &&
         loginModel.getPassword().equals("123")) {
+            model.addAttribute("mensaje", "Bienvenido: "
+                    + loginModel.getUsuario());
             return "home";
         }
+        model.addAttribute("mensaje", "Usuario y/o " +
+                "password incorrecto");
+        model.addAttribute("mostrarMensaje", true);
         return "login";
     }
 }
